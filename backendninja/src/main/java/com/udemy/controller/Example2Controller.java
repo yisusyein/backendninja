f70fbc8aed58efdea234d1f71cf7501b962ae214
jesus santiago <yisusyein@hotmail.com>
2017-05-03 19:48:59 -0500

    request1

---
+++ backendninja/src/main/java/com/udemy/controller/Example2Controller.java
@@ -0,0 +1,23 @@
+package com.udemy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/example2")
+public class Example2Controller {
+
+	private static final String EXAMPLE2_VIEW = "example2"; 
+	
+	//localhost:8080/example2/request1?nm=JON
+	//localhost:8080/example2/request1?nm=MIKEL
+	@GetMapping("/request1")
+	public ModelAndView request1(@RequestParam(name="nm") String name){
+		ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
+		mav.addObject("nm_in_model",name);
+		return mav;
+	}
+}
